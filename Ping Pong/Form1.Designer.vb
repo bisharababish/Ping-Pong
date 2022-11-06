b023e6bed628dbf37157db147c4ccbb9a8519a34
bisharababish <62569803+bisharababish@users.noreply.github.com>
2022-11-06 18:49:46 +0200

    master

---
+++ Ping Pong/Form1.Designer.vb
@@ -0,0 +1,196 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.p1 = New System.Windows.Forms.PictureBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.p2 = New System.Windows.Forms.PictureBox()
+        Me.ball = New System.Windows.Forms.PictureBox()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer3 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer4 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer5 = New System.Windows.Forms.Timer(Me.components)
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox4 = New System.Windows.Forms.PictureBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        CType(Me.p1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.p2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.ball, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'p1
+        '
+        Me.p1.BackColor = System.Drawing.Color.Red
+        Me.p1.Location = New System.Drawing.Point(28, 170)
+        Me.p1.Name = "p1"
+        Me.p1.Size = New System.Drawing.Size(10, 63)
+        Me.p1.TabIndex = 0
+        Me.p1.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.ForeColor = System.Drawing.Color.White
+        Me.Label1.Location = New System.Drawing.Point(59, 32)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(13, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "0"
+        '
+        'p2
+        '
+        Me.p2.BackColor = System.Drawing.Color.Blue
+        Me.p2.Location = New System.Drawing.Point(700, 170)
+        Me.p2.Name = "p2"
+        Me.p2.Size = New System.Drawing.Size(10, 63)
+        Me.p2.TabIndex = 2
+        Me.p2.TabStop = False
+        '
+        'ball
+        '
+        Me.ball.BackColor = System.Drawing.Color.White
+        Me.ball.Location = New System.Drawing.Point(332, 191)
+        Me.ball.Name = "ball"
+        Me.ball.Size = New System.Drawing.Size(15, 15)
+        Me.ball.TabIndex = 3
+        Me.ball.TabStop = False
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 10
+        '
+        'Timer2
+        '
+        Me.Timer2.Interval = 10
+        '
+        'Timer3
+        '
+        Me.Timer3.Interval = 10
+        '
+        'Timer4
+        '
+        Me.Timer4.Interval = 10
+        '
+        'Timer5
+        '
+        Me.Timer5.Enabled = True
+        Me.Timer5.Interval = 10
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackColor = System.Drawing.Color.Black
+        Me.PictureBox1.Location = New System.Drawing.Point(-6, -5)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(10, 424)
+        Me.PictureBox1.TabIndex = 4
+        Me.PictureBox1.TabStop = False
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.BackColor = System.Drawing.Color.Black
+        Me.PictureBox2.Location = New System.Drawing.Point(738, -5)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(10, 424)
+        Me.PictureBox2.TabIndex = 5
+        Me.PictureBox2.TabStop = False
+        '
+        'PictureBox3
+        '
+        Me.PictureBox3.BackColor = System.Drawing.Color.Black
+        Me.PictureBox3.Location = New System.Drawing.Point(-5, 410)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(753, 10)
+        Me.PictureBox3.TabIndex = 6
+        Me.PictureBox3.TabStop = False
+        '
+        'PictureBox4
+        '
+        Me.PictureBox4.BackColor = System.Drawing.Color.Black
+        Me.PictureBox4.Location = New System.Drawing.Point(-6, -5)
+        Me.PictureBox4.Name = "PictureBox4"
+        Me.PictureBox4.Size = New System.Drawing.Size(753, 10)
+        Me.PictureBox4.TabIndex = 7
+        Me.PictureBox4.TabStop = False
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.ForeColor = System.Drawing.Color.White
+        Me.Label2.Location = New System.Drawing.Point(637, 32)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(13, 13)
+        Me.Label2.TabIndex = 8
+        Me.Label2.Text = "0"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.ClientSize = New System.Drawing.Size(745, 418)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.PictureBox4)
+        Me.Controls.Add(Me.PictureBox3)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.ball)
+        Me.Controls.Add(Me.p2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.p1)
+        Me.Name = "Form1"
+        Me.Text = "Ping Pong"
+        CType(Me.p1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.p2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.ball, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents p1 As PictureBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents p2 As PictureBox
+    Friend WithEvents ball As PictureBox
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Timer2 As Timer
+    Friend WithEvents Timer3 As Timer
+    Friend WithEvents Timer4 As Timer
+    Friend WithEvents Timer5 As Timer
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents PictureBox3 As PictureBox
+    Friend WithEvents PictureBox4 As PictureBox
+    Friend WithEvents Label2 As Label
+End Class
---
+++ Ping Pong/Form1.vb
@@ -0,0 +1,104 @@
+﻿Public Class Form1
+    Dim xspeed As Double = 4
+    Dim yspeed As Double = 4
+    Dim p1s, p2s As Integer
+    Private Sub Timer5_Tick(sender As Object, e As EventArgs) Handles Timer5.Tick
+        ball.Top += yspeed
+        ball.Left -= xspeed
+
+        If ball.Bounds.IntersectsWith(p1.Bounds) Then
+            If xspeed > 0 Then
+                xspeed += 0.25
+                xspeed = -xspeed
+            Else
+                xspeed = -xspeed
+                xspeed += 0.25
+            End If
+
+        ElseIf ball.Bounds.IntersectsWith(p2.Bounds) Then
+            If xspeed > 0 Then
+                xspeed += 0.25
+                xspeed = -xspeed
+            Else
+                xspeed = -xspeed
+                xspeed += 0.25
+            End If
+        End If
+
+
+        If ball.Bounds.IntersectsWith(PictureBox3.Bounds) Then
+            yspeed = -yspeed
+        ElseIf ball.Bounds.IntersectsWith(PictureBox4.Bounds) Then
+            yspeed = -yspeed
+        ElseIf ball.Bounds.IntersectsWith(PictureBox1.Bounds) Then
+            xspeed = -xspeed
+            yspeed = -yspeed
+            p2s += 1
+            restart_game()
+        ElseIf ball.Bounds.IntersectsWith(PictureBox2.Bounds) Then
+            xspeed = -xspeed
+            yspeed = -yspeed
+            p1s += 1
+            restart_game()
+        End If
+
+
+    End Sub
+    Private Sub restart_game()
+        ball.Left = 332
+        ball.Top = 191
+        Label1.Text = p1s
+        Label2.Text = p2s
+        xspeed = 4
+        yspeed = 4
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        If p1.Top <= 347 Then
+            p1.Top += 5
+        End If
+    End Sub
+
+    Private Sub Timer2_Tick(sender As Object, e As EventArgs) Handles Timer2.Tick
+        If p1.Top >= 5 Then
+            p1.Top -= 5
+        End If
+    End Sub
+
+    Private Sub Timer4_Tick(sender As Object, e As EventArgs) Handles Timer4.Tick
+        If p2.Top <= 347 Then
+            p2.Top += 5
+        End If
+    End Sub
+
+    Private Sub Timer3_Tick(sender As Object, e As EventArgs) Handles Timer3.Tick
+        If p2.Top >= 5 Then
+            p2.Top -= 5
+        End If
+    End Sub
+
+    Private Sub Form1_KeyDown(sender As Object, e As KeyEventArgs) Handles MyBase.KeyDown
+        If e.KeyCode = Keys.Up Then
+            Timer3.Enabled = True
+        ElseIf e.KeyCode = Keys.Down Then
+            Timer4.Enabled = True
+        ElseIf e.KeyCode = Keys.S Then
+            Timer1.Enabled = True
+        ElseIf e.KeyCode = Keys.W Then
+            Timer2.Enabled = True
+        End If
+    End Sub
+
+    Private Sub Form1_KeyUp(sender As Object, e As KeyEventArgs) Handles MyBase.KeyUp
+        If e.KeyCode = Keys.Up Then
+            Timer3.Enabled = False
+        ElseIf e.KeyCode = Keys.Down Then
+            Timer4.Enabled = False
+        ElseIf e.KeyCode = Keys.S Then
+            Timer1.Enabled = False
+        ElseIf e.KeyCode = Keys.W Then
+            Timer2.Enabled = False
+        End If
+    End Sub
+
+End Class
